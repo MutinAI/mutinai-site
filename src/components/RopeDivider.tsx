7330f0a5a92f4fe57dc9f3efb2626b5ccc64e5c0
MutinAI <ops@mutinai.proton.me>
2025-06-02 13:44:51 -0400

    change divider back

--- src/components/RopeDivider.tsx
+++ src/components/RopeDivider.tsx
@@ -1,7 +1,11 @@
 export default function RopeDivider() {
   return (
     <div className="flex justify-center my-12">
-      <img src="/assets/rope.png" alt="" className="h-6 w-6" />
+      <img
+        src="/assets/rope.png"
+        alt=""
+        className="h-3 w-full max-w-md object-cover"
+      />
     </div>
   );
 }
